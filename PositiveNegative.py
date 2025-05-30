a8ea7a308ed48937476e11c0d4df46afa9e4a1b0
divyaingulkar10 <divyaingulkar484@gmail.com>
2025-05-30 11:15:14 +0530

    Add files via upload

---
+++ PositiveNegative.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 24 09:39:12 2025
+
+@author: Admin
+"""
+
+n=int(input("enter number: "))
+if n>0:
+    print("number is positive")
+else:
+    print("number is nagative")    
