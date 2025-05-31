7a288e07adbd1481686f0d584a139aa853205385
divyaingulkar10 <divyaingulkar484@gmail.com>
2025-05-31 09:45:39 +0530

    Add files via upload

---
+++ swappingVariable.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 11 09:41:34 2025
+
+@author: Admin
+"""
+
+a=int(input("enter num a:"))
+b=int(input("enter num b:"))
+temp=a
+a=b
+b=temp
+print("swapping a",a)
+print("swapping b",b)
